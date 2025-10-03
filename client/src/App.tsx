57853adfe36b4d3dcf77a3a784acdfb5549eb905
Sam Edwards <samedwards@Sams-MacBook-Pro-2.local>
2025-10-03 09:49:04 -0400

    started client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+    return (
+        <div>
+            This is the app.
+        </div>
+    )
+}
+
+export default App
---
+++ client/src/index.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client"
+import App from './App'
+import { StrictMode } from "react"
+
+document.body.innerHTML = "<div id='app'></div>"
+
+const appElement = document.getElementById("app")
+
+if (!appElement) 
+    throw new Error("No app element found")
+
+const root = createRoot(appElement)
+root.render(<StrictMode><App /></StrictMode>)
